dc5fef5d5d6c660226f08f0dbeb3926ddb7781dc
Denny Rodriguez Fajardo <dennitorf@gmail.com>
2022-03-27 22:33:43 -0400

    adding doming layer and project references

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreatedBy { set; get; }
+        public DateTime CreatedData { get; set; }   
+        public string LastModifiedBy { set; get; }
+        public DateTime? LastModifiedDate { set; get; }
+    }
+}
